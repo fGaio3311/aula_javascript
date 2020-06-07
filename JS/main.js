a85d6c3ab55b8cb20797134b528cb887d5c30827
fGaio3311 <61062206+fGaio3311@users.noreply.github.com>
2020-06-06 22:28:46 -0300

    Primeira_aula_de_javascript_DIO

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+var nome = "Filipe Gaio";
+var idade = 21;
+var idade2 = 2;
+var frase = "Bom dia Brasil";
+
+//alert ("Bem vindo" + nome);
+//alert (nome + "tem" + idade + "anos");
+//alert (idade + idade2);
+
+console.log (nome);
+console.log (idade + idade2);
+console.log (frase.toLowerCase());
+// toLowerCase diminui todos os caracteres ficam em letra minúscula
+// to
